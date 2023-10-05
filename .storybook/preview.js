c7ce183baa8825a4643bf68d9f61698769f60844
Francis Thibault <francis.thibault@gmail.com>
2023-10-05 15:00:08 -0400

    added a preview head to try to fix font loading

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -13,19 +13,6 @@ import { Themes } from "./styles/themes";
 //     import("@css/font/index.css");
 // }
 
-// Use the document.fonts API to check if fonts have loaded
-// https://developer.mozilla.org/en-US/docs/Web/API/Document/fonts API to
-const fontLoader = async () => ({
-    fonts: await document.fonts.ready,
-});
-
-/* 👇 It's configured as a global loader
-* See https://storybook.js.org/docs/react/writing-stories/loaders
-* to learn more about loaders
-*/
-export const loaders = isChromatic() && document.fonts ? [fontLoader] : [];
-
-
 export const parameters = {
     options: {
         storySort: {
