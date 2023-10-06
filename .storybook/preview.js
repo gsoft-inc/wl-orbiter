512d43ff7963f95ff833f8d8bd64cac1da188a28
Francis Thibault <francis.thibault@gmail.com>
2023-10-06 12:09:16 -0400

    Revert "added font declaration for Chromatic"
    This reverts commit 2fd73f8a3f4ca4690e30c01ea77543dcf6e12dd2.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -8,12 +8,10 @@ import { withBackgroundMatchingColorScheme, withCenteredCanvas, withDocsContaine
 import { Code, Highlight } from "@stories/mdx";
 import { Themes } from "./styles/themes";
 
-if (!isChromatic) {
-    // Custom font makes chromatic inconsistent and cause "false positive". View https://www.chromatic.com/docs/resource-loading#loading-custom-fonts.
-    import("@css/font/index.css");
-} else {
-    import("./styles/chromatic.css");
-}
+// if (isChromatic) {
+//     // Custom font makes chromatic inconsistent and cause "false positive". View https://www.chromatic.com/docs/resource-loading#loading-custom-fonts.
+//     import("@css/font/index.css");
+// }
 
 export const parameters = {
     options: {
