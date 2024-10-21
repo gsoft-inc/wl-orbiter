b699453e067a1c51d8a0bdc60071deffcebdf5ba
Alexandre Asselin <alexandre.asselin@gsoft.com>
2024-10-21 09:31:53 -0400

    revert testing library to test the CI

--- setupTests.ts
+++ setupTests.ts
@@ -1,5 +1,7 @@
-import "@testing-library/jest-dom";
-import "@testing-library/jest-dom/jest-globals";
+// import "@testing-library/jest-dom";
+// import "@testing-library/jest-dom/jest-globals";
+
+import "@testing-library/jest-dom/extend-expect";
 
 global.ResizeObserver = require("resize-observer-polyfill");
 
