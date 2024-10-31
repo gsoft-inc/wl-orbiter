e802462d66b8d63862352a023275063e533adb9d
Victor Trinh <victor.trinh@gsoft.com>
2024-10-31 14:58:03 -0400

    Anchor

---
+++ packages/components/src/html/docs/anchor.stories.tsx
@@ -0,0 +1,19 @@
+import { A } from "@components/html";
+import { Meta, StoryObj } from "@storybook/react";
+
+const meta = {
+    title: "Html elements/A",
+    component: A,
+    id: "html-anchor"
+} as Meta<typeof A>;
+
+export default meta;
+
+type Story = StoryObj<typeof A>;
+
+export const Example: Story = {
+    render: () => (
+        <A color="neutral-weak" href="https://www.cc.com/shows/futurama">Futurama</A>
+    )
+};
+
