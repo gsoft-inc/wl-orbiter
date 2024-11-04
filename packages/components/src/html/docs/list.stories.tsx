9416a15f985472b42a06045f3a4e18ad3503599e
Victor Trinh <victor.trinh@gsoft.com>
2024-11-04 10:15:43 -0500

    List

---
+++ packages/components/src/html/docs/list.stories.tsx
@@ -0,0 +1,22 @@
+import { LI, UL } from "@components/html";
+import { Meta, StoryObj } from "@storybook/react";
+
+const meta = {
+    title: "Html elements/List",
+    component: UL,
+    id: "html-list"
+} as Meta<typeof UL>;
+
+export default meta;
+
+type Story = StoryObj<typeof LI>;
+
+export const Example: Story = {
+    render: () => (
+        <UL color="neutral-weak">
+            <LI>Explore</LI>
+            <LI>Discover</LI>
+            <LI color="sapphire-600">Colonize</LI>
+        </UL>
+    )
+};
