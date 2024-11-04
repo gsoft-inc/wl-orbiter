452e0d8b64009b87f5e0b0a8b938ec49d089887a
Victor Trinh <victor.trinh@gsoft.com>
2024-11-04 10:07:20 -0500

    Img

---
+++ packages/components/src/html/docs/img.stories.tsx
@@ -0,0 +1,19 @@
+import { Img } from "@components/html";
+import { Meta, StoryObj } from "@storybook/react";
+import { Planet } from "./assets";
+
+const meta = {
+    title: "Html elements/Img",
+    component: Img,
+    id: "html-img"
+} as Meta<typeof Img>;
+
+export default meta;
+
+type Story = StoryObj<typeof Img>;
+
+export const Example: Story = {
+    render: () => (
+        <Img border="rock-400" src={Planet} />
+    )
+};
