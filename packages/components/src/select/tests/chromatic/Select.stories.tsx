06101b10d95666c08e13a7011fdd48e8dc3cb2ad
Alexandre Asselin <alexandre.asselin@gsoft.com>
2024-11-05 15:09:40 -0500

    update select issues

--- packages/components/src/select/tests/chromatic/Select.stories.tsx
+++ packages/components/src/select/tests/chromatic/Select.stories.tsx
@@ -1,5 +1,5 @@
 import { Inline, Stack } from "@components/layout";
-import { Select, useSelect } from "@components/select";
+import { Select, useSelect, type SelectProps } from "@components/select";
 
 import { Button } from "@components/button";
 import { Div } from "@components/html";
@@ -91,13 +91,15 @@ export const Styling: SelectStory = {
     )
 };
 
+
+
 function CustomSelect({
     open,
     placeholder,
     "aria-label": ariaLabel,
     children,
     ...rest
-}) {
+}: SelectProps) {
     const { selectedItem, triggerProps, overlayProps, listboxProps } = useSelect(children, {
         open,
         ariaLabel
@@ -110,7 +112,7 @@ function CustomSelect({
                 {...triggerProps}
                 variant="secondary"
             >
-                {selectedItem?.content ?? placeholder}
+                {selectedItem?.text ?? placeholder}
             </Button>
             <Overlay {...overlayProps}>
                 <Listbox {...listboxProps} />
