7249dadbe3119b1e09cb776e62fd0c71eedf3fce
Victor Trinh <victor.trinh@gsoft.com>
2024-11-08 14:31:18 -0500

    Fix lint

--- .storybook/mdx/typography/Heading.tsx
+++ .storybook/mdx/typography/Heading.tsx
@@ -1,8 +1,9 @@
+import React from "react";
 import { mergeProps } from "../../../packages/components/src/shared";
 
-export const H1 = props => <h1 {...mergeProps(props, { className: "orbiter-doc-h1" })} />;
-export const H2 = props => <h2 {...mergeProps(props, { className: "orbiter-doc-h2" })} />;
-export const H3 = props => <h3 {...mergeProps(props, { className: "orbiter-doc-h3" })} />;
-export const H4 = props => <h4 {...mergeProps(props, { className: "orbiter-doc-h4" })} />;
-export const H5 = props => <h5 {...mergeProps(props, { className: "orbiter-doc-h5" })} />;
-export const H6 = props => <h6 {...mergeProps(props, { className: "orbiter-doc-h6" })} />;
+export const H1 = ({ children, ...props }) => <h1 {...mergeProps(props, { className: "orbiter-doc-h1" })}>{children}</h1>;
+export const H2 = ({ children, ...props }) => <h2 {...mergeProps(props, { className: "orbiter-doc-h2" })}>{children}</h2>;
+export const H3 = ({ children, ...props }) => <h3 {...mergeProps(props, { className: "orbiter-doc-h3" })}>{children}</h3>;
+export const H4 = ({ children, ...props }) => <h4 {...mergeProps(props, { className: "orbiter-doc-h4" })}>{children}</h4>;
+export const H5 = ({ children, ...props }) => <h5 {...mergeProps(props, { className: "orbiter-doc-h5" })}>{children}</h5>;
+export const H6 = ({ children, ...props }) => <h6 {...mergeProps(props, { className: "orbiter-doc-h6" })}>{children}</h6>;
